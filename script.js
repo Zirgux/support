9175ed163c9eb70343af823824a9f5974800bfa0
Štěpán <97223180+Zirgux@users.noreply.github.com>
2026-01-12 12:37:33 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 // Přepínač: zatím bez backendu
-const USE_BACKEND = false;
+const USE_BACKEND = true;
 
 // TADY si každý student nastaví svoje údaje (až bude backend):
-const BACKEND_URL = "https://lkostak.onrender.com/chat";
-const STUDENT_ID = "Kostak_Ladislav_4A";
+const BACKEND_URL = "https://support-52a2.onrender.com/chat";
+const STUDENT_ID = "Brzak_Stepan_4ITA";
 
 const form = document.getElementById("question-form");
 const textarea = document.getElementById("question");
